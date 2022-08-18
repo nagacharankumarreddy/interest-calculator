c09528ced98480c701420a1fbce80d017e10e189
nagacharan <nagacharankumarreddy@gmail.com>
2022-08-19 02:24:26 +0530

    Updates

---
+++ precache-manifest.2adf169160e6a3bb98c754ccbcb8a891.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f8da6cc3ac0703ecc3e",
+    "url": "/interest-calculator/static/css/main.90393930.chunk.css"
+  },
+  {
+    "revision": "5f8da6cc3ac0703ecc3e",
+    "url": "/interest-calculator/static/js/main.5f8da6cc.chunk.js"
+  },
+  {
+    "revision": "2bc649a667ccbd873989",
+    "url": "/interest-calculator/static/js/1.2bc649a6.chunk.js"
+  },
+  {
+    "revision": "a4b4e4e8cac665d5b321",
+    "url": "/interest-calculator/static/css/2.1f6f576b.chunk.css"
+  },
+  {
+    "revision": "a4b4e4e8cac665d5b321",
+    "url": "/interest-calculator/static/js/2.a4b4e4e8.chunk.js"
+  },
+  {
+    "revision": "a56e090d12fe14ad59c4",
+    "url": "/interest-calculator/static/js/runtime~main.a56e090d.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/interest-calculator/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "f77603a4cc84b54c4cae9836fdcc763a",
+    "url": "/interest-calculator/index.html"
+  }
+];
